044ba79baef8ac9b4617df184411334cfa69b642
NathanielFactor <bbnat123@outlook.com>
2024-11-22 16:50:12 -0500

    finished testing arduino, ready to go for codejam

--- Testing/blinkingLEDS/blinkingLEDS.ino
+++ Testing/blinkingLEDS/blinkingLEDS.ino
@@ -1,9 +1,12 @@
+int LEDpin = 13;
+int delayT = 1000;
+
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(LEDpin, OUTPUT);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  digitalWrite(LEDpin, HIGH);
+  delay(delayT);
+  digitalWrite(LEDpin, LOW);
+  delay(delayT);
 }
